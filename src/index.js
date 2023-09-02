6c1347b1175fa5a1328a6eaee31f50d62a8404af
splex7 <splex7@gmail.com>
2023-09-02 17:25:08 +0900

    applied responsive

--- src/index.js
+++ src/index.js
@@ -7,6 +7,32 @@ class ReactSlickDemo extends React.Component {
   render() {
     var settings = {
       dots: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      mobileFirst: true,
+      pauseOnHover: true,
+      fade: true,
+      responsive: [
+        // 반응형 웹 구현 옵션
+        {
+          breakpoint: 960, //화면 사이즈 960px
+          settings: {
+            //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 768, //화면 사이즈 768px
+          settings: {
+            //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+            slidesToShow: 2,
+          },
+        },
+      ],
     };
     return (
       <div className="container">
@@ -18,10 +44,16 @@ class ReactSlickDemo extends React.Component {
             />
           </div>
           <div>
-            <img src="http://placekitten.com/g/400/200" alt="" />
+            <img
+              src="https://res.cloudinary.com/glide/image/fetch/f_auto,w_1200,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FHstkNlpikcMbVABdiNZo-tutorial-beginner%2Fpub%2FpqbWzOZlJQteTVWOKvUI.jpg"
+              alt=""
+            />
           </div>
           <div>
-            <img src="http://placekitten.com/g/400/200" alt="" />
+            <img
+              src="https://res.cloudinary.com/glide/image/fetch/f_auto,w_1200,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FHstkNlpikcMbVABdiNZo-tutorial-beginner%2Fpub%2FX0i6S9iiMX7AE5WDTb7r.jpg"
+              alt=""
+            />
           </div>
         </Slider>
       </div>
